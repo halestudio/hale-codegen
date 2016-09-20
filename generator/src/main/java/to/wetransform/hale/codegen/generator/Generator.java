d774af4c85ed60957cabfdaa3e374d2d331162c4
Simon Templer <simon@wetransform.to>
2016-09-20 14:10:44 +0200

    first version of group (sequence/choice) support

--- generator/src/main/java/to/wetransform/hale/codegen/generator/Generator.java
+++ generator/src/main/java/to/wetransform/hale/codegen/generator/Generator.java
@@ -1,5 +1,6 @@
 package to.wetransform.hale.codegen.generator;
 
+import java.awt.Choice;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
@@ -27,13 +28,18 @@ import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
+import eu.esdihumboldt.hale.common.instance.model.Group;
 import eu.esdihumboldt.hale.common.schema.model.ChildDefinition;
+import eu.esdihumboldt.hale.common.schema.model.DefinitionGroup;
+import eu.esdihumboldt.hale.common.schema.model.GroupPropertyDefinition;
 import eu.esdihumboldt.hale.common.schema.model.PropertyDefinition;
 import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import eu.esdihumboldt.hale.common.schema.model.constraint.property.Cardinality;
+import eu.esdihumboldt.hale.common.schema.model.constraint.property.ChoiceFlag;
 import eu.esdihumboldt.hale.common.schema.model.constraint.type.Binding;
 import eu.esdihumboldt.hale.common.schema.model.constraint.type.HasValueFlag;
 import to.wetransform.hale.codegen.model.Named;
+import to.wetransform.hale.codegen.model.Property;
 import to.wetransform.hale.codegen.model.Value;
 
 public class Generator {
@@ -41,6 +47,7 @@ public class Generator {
   private static final Logger log = LoggerFactory.getLogger(Generator.class);
 
   private final Map<QName, ClassName> typeClasses = new HashMap<>();
+  private final Map<QName, ClassName> groupClasses = new HashMap<>();
   private final String packagePrefix = ""; //TODO configurable
   private File targetFolder;
 
@@ -69,6 +76,8 @@ public class Generator {
 
     // generate class
     className = newClassName(type.getName());
+    typeClasses.put(type.getName(), className); // being generated
+
     TypeSpec.Builder builder = TypeSpec.classBuilder(className);
 
     // add named annotation
@@ -104,7 +113,39 @@ public class Generator {
 
     javaFile.writeTo(targetFolder);
 
-    typeClasses.put(type.getName(), className);
+    return className;
+  }
+
+  private TypeName getOrCreateGroupType(GroupPropertyDefinition group) throws IOException {
+    //XXX is there potential to reuse group types?
+    //XXX identifier right choice for index
+    ClassName className = groupClasses.get(group.getName());
+
+    if (className != null) {
+      // class was already generated
+      return className;
+    }
+
+    groupClasses.put(group.getName(), className); // being generated...
+
+    // generate class
+    className = newClassName(group.getName()); //XXX is the name sufficient?
+    TypeSpec.Builder builder = TypeSpec.classBuilder(className);
+
+    // add named annotation
+    //XXX groups don't have resolvable names
+//    builder.addAnnotation(createNameAnnotation(group.getName()));
+
+    //TODO mark as group or choice?
+
+    // add properties
+    //XXX makes sense for a sequence, but can we provide a better interface for choices?
+    addProperties(group, builder);
+
+    TypeSpec typeClass = builder.build();
+    JavaFile javaFile = JavaFile.builder(className.packageName(), typeClass).build();
+
+    javaFile.writeTo(targetFolder);
     return className;
   }
 
@@ -126,18 +167,21 @@ public class Generator {
     }
   }
 
-  private void addProperties(TypeDefinition type, TypeSpec.Builder builder) throws IOException {
+  private void addProperties(DefinitionGroup type, TypeSpec.Builder builder) throws IOException {
+    //TODO special handling for restriction types? seems properties are repeatedly declared in subtypes there
     for (ChildDefinition<?> child : type.getDeclaredChildren()) {
       addProperty(builder, child);
     }
   }
 
   private void addProperty(TypeSpec.Builder builder, ChildDefinition<?> child) throws IOException {
+    TypeName propertyType;
+    String propertyName = getPropertyName(child.getName());
+    Cardinality card;
+
     if (child.asProperty() != null) {
       PropertyDefinition property = child.asProperty();
-      String propertyName = getPropertyName(property.getName());
 
-      TypeName propertyType;
       if (isSimpleType(property.getPropertyType())) {
         // simple type -> use binding
         Class<?> bindingClass = property.getPropertyType().getConstraint(Binding.class).getBinding();
@@ -150,19 +194,24 @@ public class Generator {
         propertyType = getOrCreateClass(property.getPropertyType());
       }
 
-      Cardinality card = property.getConstraint(Cardinality.class);
-      if (card.mayOccurMultipleTimes()) {
-        addCollectionProperty(builder, propertyName, propertyType, child.getName());
-      }
-      else {
-        addBeanProperty(builder, propertyName, propertyType, child.getName());
-      }
+      card = property.getConstraint(Cardinality.class);
     }
     else if (child.asGroup() != null) {
-      //TODO
+      GroupPropertyDefinition group = child.asGroup();
+
+      propertyType = getOrCreateGroupType(group);
+
+      card = group.getConstraint(Cardinality.class);
     }
     else {
       throw new IllegalStateException("Unsupported child definition type");
+    }
+
+    if (card.mayOccurMultipleTimes()) {
+      addCollectionProperty(builder, propertyName, propertyType, child);
+    }
+    else {
+      addBeanProperty(builder, propertyName, propertyType, child);
     }
   }
 
@@ -172,12 +221,16 @@ public class Generator {
   }
 
   private void addBeanProperty(TypeSpec.Builder builder, String propertyName, TypeName propertyType,
-      QName qualifiedName) {
+      ChildDefinition<?> definition) {
+    QName qualifiedName = (definition == null) ? (null) : (definition.getName());
+
     // add the field
-    FieldSpec field = FieldSpec.builder(propertyType, propertyName, Modifier.PRIVATE)
-        .addAnnotation(createNameAnnotation(qualifiedName))
-        .build();
-      builder.addField(field);
+    FieldSpec.Builder fieldBuilder = FieldSpec.builder(propertyType, propertyName, Modifier.PRIVATE)
+        .addAnnotation(createNameAnnotation(qualifiedName));
+    if (definition != null) {
+      fieldBuilder.addAnnotation(getPropertyAnnotation(definition));
+    }
+    builder.addField(fieldBuilder.build());
 
     // add setter
     String setterName = "set" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1);
@@ -198,17 +251,36 @@ public class Generator {
     builder.addMethod(getter);
   }
 
+  private AnnotationSpec getPropertyAnnotation(ChildDefinition<?> definition) {
+    Class<?> type = Property.class;
+
+    if (definition.asGroup() != null) {
+      if (definition.asGroup().getConstraint(ChoiceFlag.class).isEnabled()) {
+        type = Choice.class;
+      }
+      else {
+        type = Group.class;
+      }
+    }
+
+    return AnnotationSpec.builder(type).build();
+  }
+
   private void addCollectionProperty(TypeSpec.Builder builder, String propertyName, TypeName propertyType,
-      QName qualifiedName) {
+      ChildDefinition<?> definition) {
+    QName qualifiedName = (definition == null) ? (null) : (definition.getName());
+
     propertyType = ParameterizedTypeName.get(ClassName.get(List.class), propertyType);
 
     // add the field
-    FieldSpec field = FieldSpec.builder(propertyType, propertyName, Modifier.PRIVATE)
-      // initialize with empty collection
-      .initializer("new $T()", ClassName.get(ArrayList.class))
-      .addAnnotation(createNameAnnotation(qualifiedName))
-      .build();
-    builder.addField(field);
+    FieldSpec.Builder fieldBuilder = FieldSpec.builder(propertyType, propertyName, Modifier.PRIVATE)
+        // initialize with empty collection
+        .initializer("new $T()", ClassName.get(ArrayList.class))
+        .addAnnotation(createNameAnnotation(qualifiedName));
+    if (definition != null) {
+      fieldBuilder.addAnnotation(getPropertyAnnotation(definition));
+    }
+    builder.addField(fieldBuilder.build());
 
     //TODO collection optimized methods like add()?
 
@@ -235,7 +307,11 @@ public class Generator {
     // TODO Auto-generated method stub
     //FIXME collisions in classes and their super classes need to be avoided
     // -> use scoped NameAllocator and add "value", then super type properties, then properties... ?
-    return name.getLocalPart();
+    String result = NameAllocator.toJavaIdentifier(name.getLocalPart());
+    if ("null".equals(result)) {
+      return "_null";
+    }
+    return result;
   }
 
   private ClassName newClassName(QName name) {
